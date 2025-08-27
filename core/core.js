dd46a199d23780e3e644ec4c075b947eac7f2b08
DSSalvatore-Development <D.S.Salvatore@iCloud.com>
2025-08-27 17:45:49 +0700

    Delete core directory

--- core/core.js
+++
@@ -1,9 +0,0 @@
-// Ядро проекта
-window.Core = {
-  init() {
-    console.log("Ядро инициализировано.");
-    UI.showNotification("Ядро", "Инициализация завершена.");
-    ModuleLoader.loadModules();
-  }
-};
-document.addEventListener("DOMContentLoaded", Core.init);
