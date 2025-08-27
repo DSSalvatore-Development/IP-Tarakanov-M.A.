465f422d75648fa1c2da8a163e1b9301581b4e94
DSSalvatore-Development <D.S.Salvatore@iCloud.com>
2025-08-27 16:36:51 +0700

    Delete core-ui.js

--- core-ui.js
+++
@@ -1,9 +0,0 @@
-// UI-функции ядра
-window.CoreUI = {
-  showError: function(msg) {
-    CoreUtils.notify("Ошибка: " + msg, "error");
-  },
-  showInfo: function(msg) {
-    CoreUtils.notify(msg, "info");
-  }
-};
